a9b5503001d8992c6d0df9b0f4a32feb5e6a66d5
tkooooo123 <tkooooo123@gmail.com>
2022-01-10 19:02:53 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+const port = 3000
+
+// setting template engine
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) =>{    
+    res.render('index', {restaurant: restaurantList.results})
+})
+app.get('/restaurants/:restaurant_id', (req, res) => {
+    console.log('req.params.resraurant_id', req.params.restaurant_id)
+    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+    res.render('show', {restaurant: restaurant})
+})
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`Express is listening on localhost:${port}`)
+})
