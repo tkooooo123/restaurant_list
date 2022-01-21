24c83508e5cd9cb9cff91e16a537f07259564526
tkooooo123 <tkooooo123@gmail.com>
2022-01-21 18:38:17 +0800

    新增 增加一筆餐廳資料功能

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const exphbs = require('express-handlebars')
 //const restaurantList = require('./restaurant.json')
 const mongoose = require('mongoose')  //載入mongoose
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
-const Restaurant = require('./models/restaurant')
+const Restaurant = require('./models/Restaurant')
 
 mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到DB
 const db = mongoose.connection  //取得資料庫連線狀態
@@ -20,12 +21,27 @@ db.once('open', () => {
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) =>{    
     Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', {restaurants}))
     .catch(error => console.log(error))
 })
+
+//新增一筆資料
+app.get('/restaurants/new', (req, res) => {
+    return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+        
+    return Restaurant.create(req.body)     // 存入資料庫
+      .then(() => res.redirect('/')) // 新增完成後導回首頁
+      .catch(error => console.log(error))
+  })
 
 app.get ('/search', (req,res) => {
     const keyword = req.query.keyword
@@ -42,6 +58,7 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 })
 
 app.use(express.static('public'))
+
 
 app.listen(port, () => {
     console.log(`Express is listening on localhost:${port}`)
