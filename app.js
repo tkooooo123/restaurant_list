382ee8e81a3f1178a3615a84f3ccd32ae44cde4a
tkooooo123 <tkooooo123@gmail.com>
2022-01-21 21:30:30 +0800

    新增 刪除一筆餐廳資料功能

--- app.js
+++ app.js
@@ -68,7 +68,14 @@ app.post('/restaurants/:id', (req, res) => {
     .then(() => res.redirect(`/restaurants/${id}/`))
     .catch(error => console.log(error))
 })
-
+//刪除一筆餐廳資料
+app.post('/restaurants/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 
 app.get ('/search', (req,res) => {
