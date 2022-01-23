456343fe3605eff910fb8cf6e87ceaf556cd1186
tkooooo123 <tkooooo123@gmail.com>
2022-01-23 21:01:46 +0800

    新增排序功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,9 +6,19 @@ const Restaurant = require('../../models/Restaurant')
 router.get('/', (req, res) =>{    
     Restaurant.find()
     .lean()
+    .sort()
     .then(restaurants => res.render('index', {restaurants}))
     .catch(error => console.log(error))
 })
+router.get('/:sort', (req, res) => {
+    const sort = req.params.sort
+   
+    Restaurant.find()   
+      .lean()
+      .sort(`${sort}`)
+      .then(restaurants => res.render('index', { restaurants, sort }))
+      .catch(error => console.log(error))
+  })
 router.get ('/search', (req,res) => {
     const keyword = req.query.keyword.trim().toLowerCase()
     Restaurant.find({$or: [{name: { $regex: keyword, $options: 'i' }}, {category: { $regex: keyword, $options: 'i' }}]})
