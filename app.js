218fdc843071c0ffebb5af9bfb0fe42e5818a26f
tkooooo123 <tkooooo123@gmail.com>
2022-01-23 19:08:19 +0800

    重構 mongoose設定

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')  //載入mongoose
-const app = express()
 const bodyParser = require('body-parser')
-const port = 3000
 const Restaurant = require('./models/Restaurant')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到DB
-const db = mongoose.connection  //取得資料庫連線狀態
-//連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-//連線成功
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
+const app = express()
+const port = 3000
+
+require('./config/mongoose')
+
 // setting template engine
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-
 app.use(methodOverride('_method'))
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(routes)
-
-
-
 app.use(express.static('public'))
 
 app.listen(port, () => {
---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')  //載入mongoose
+mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到DB
+const db = mongoose.connection  //取得資料庫連線狀態
+//連線異常
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+//連線成功
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
+module.exports = db
