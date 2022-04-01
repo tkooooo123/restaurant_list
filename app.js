ce253834610ab0ae4837046c776c49b1857e130b
tkooooo123 <tkooooo123@gmail.com>
2022-04-01 22:54:45 +0800

    feat: remove test code

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(methodOverride('_method'))
 usePassport(app)
 app.use(flash())
 app.use((req, res, next) => {
-    console.log(req.user)
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
     res.locals.success_msg = req.flash('success_msg')
